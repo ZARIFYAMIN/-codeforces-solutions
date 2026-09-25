62e1fbd9610481bcf9f0d0881118d8c42c3bb7ee
Zarif Yamin <zarifdhip@gmail.com>
2026-09-25 23:06:14 +0600

    Sync Codeforces: 2263A - Min Max Game [Python 3]

---
+++ 2263A_Min_Max_Game.py
@@ -0,0 +1,12 @@
+t=int(input())
+ 
+while t>0:
+    n=int(input())
+    a=list(map(int, input().split()))
+    o=a.count(1)
+    z=n-o
+    if o>=z:
+        print("Bessie")
+    else:
+        print("Elsie")
+    t-=1
